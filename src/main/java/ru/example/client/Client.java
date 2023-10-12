875117aa826c4fd56750df04a086a8f3efc8f6ce
RinatG <rinat_galimov_1992@mail.ru>
2023-10-12 15:16:05 +0300

    Initial commit

---
+++ src/main/java/ru/example/client/Client.java
@@ -0,0 +1,36 @@
+package ru.example.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        String host = "127.0.0.1";
+        int port = 8089;
+
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            Scanner consoleReader = new Scanner(System.in);
+            while (true) {
+                //TODO Вывод пока есть строка
+                String line = in.readLine();
+                System.out.println(line);
+                String answer = consoleReader.nextLine();
+                if (answer.equals("exit")) {
+                    break;
+                }
+                out.println(answer);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
---
+++ src/main/java/ru/example/server/Server.java
@@ -0,0 +1,48 @@
+package ru.example.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private static Krestiki krestiki;
+
+    public static void main(String[] args) {
+        System.out.println("Server started");
+        int port = 8089;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                ) {
+                    krestiki = new Krestiki(out, in);
+
+                    System.out.printf("New connection accepted. Port %d%n", clientSocket.getPort());
+                    out.println("Hi, user! What's your name?");
+
+                    final String name = in.readLine();
+                    out.println(String.format("Hello %s, your port is %d", name, clientSocket.getPort()) + ". How old are you?");
+
+                    String age = in.readLine();
+                    out.println(((Integer.parseInt(age) > 18) ? "Whats up bro?" : "How do you do fellow kids?") + "  1. Fine  2. Bad");
+
+                    String stateOfMind = in.readLine();
+                    out.println(((Integer.parseInt(stateOfMind) == 1) ? "Happy for you buddy. " : "Everything will work out, don't worry. ")
+                            + "Would like to play some game with me?(yes/no)");
+
+                    String playGame = in.readLine();
+                    out.println((playGame.equalsIgnoreCase("yes")) ? "krestiki.main()" :
+                            ("Thanks for dialog, " + name + ", you can white 'exit' to leave this dialog"));
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
